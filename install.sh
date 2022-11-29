480e8be30676c21c8dfe82f1914d77283e759b26
musaubrian <musaubrian15@gmail.com>
2022-11-29 14:24:45 +0300

    chore: update install script

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 cp -r ./nvim ~/.config/nvim
 cp -r ./coc ~/.config/coc
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 cd ~/.config/coc/extensions || exit
 npm install
