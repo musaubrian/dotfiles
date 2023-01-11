10e41643f72c95b9d0d6a5369467d73fa2ca8421
musaubrian <musaubrian15@gmail.com>
2023-01-11 18:38:13 +0300

    chore: add lsp info keybinding

--- nvim/lua/ernest/remap.lua
+++ nvim/lua/ernest/remap.lua
@@ -3,3 +3,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 --make file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", {silent = true})
+-- formats the current buffer
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+-- get info on item on cursor
+vim.keymap.set('n', "K", vim.lsp.buf.hover, bufopts)
