86fe7bdf390b3f050a59da564981355a4783f19f
musaubrian <musaubrian15@gmail.com>
2023-01-11 19:16:00 +0300

    chore: add sql in cht.sh

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -1,10 +1,21 @@
+require("tokyonight").setup({
+    transparent = true, -- Enable this to disable setting the background color
+    styles = {
+        comments = { italic = true },
+        keywords = { italic = false },
+        functions = {},
+        variables = { italic = true },
+        floats = "transparent", -- style for floating windows
+    },
+    lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
+})
+
 function Mytheme(color)
 
-	color = color or "tokyonight-storm"
-	vim.cmd.colorscheme(color)
+    color = color or "tokyonight-storm"
+    vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-	vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 end
 
 Mytheme()
--- nvim/lua/ernest/remap.lua
+++ nvim/lua/ernest/remap.lua
@@ -4,6 +4,8 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 --make file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", {silent = true})
 -- formats the current buffer
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "K", vim.lsp.buf.format)
 -- get info on item on cursor
-vim.keymap.set('n', "K", vim.lsp.buf.hover, bufopts)
+vim.keymap.set('n', "<leader>i", vim.lsp.buf.hover)
+-- find where the item on cursor is referenced
+vim.keymap.set('n', "<leader>r", vim.lsp.buf.references)
