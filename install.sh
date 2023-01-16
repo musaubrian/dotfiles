ca6284c006b8eda8f668e0897d60338ce777ca62
Ernest <musaubrian15@gmail.com>
2023-01-16 16:45:59 +0300

    chore: update neovim plugins

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 mkdir ~/scripts/
 cp ./tmux.conf ~/.tmux.conf
 cp ./cht.sh ~/scripts/
-mv .nvim ~/.config/nvim	
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -1,21 +1,25 @@
 require("tokyonight").setup({
-    transparent = true, -- Enable this to disable setting the background color
-    styles = {
-        comments = { italic = true },
-        keywords = { italic = false },
-        functions = {},
-        variables = { italic = true },
-        floats = "transparent", -- style for floating windows
-    },
-    lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
+  -- your configuration comes here
+  style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  transparent = true, -- Enable this to disable setting the background color
+  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+  styles = {
+    -- Style to be applied to different syntax groups
+    -- Value is any valid attr-list value for `:help nvim_set_hl`
+    comments = { italic = true },
+    functions = {},
+    variables = {},
+    floats = "transparent", -- style for floating windows
+  },
 })
 
 function Mytheme(color)
 
-    color = color or "tokyonight-storm"
+    color = color or "tokyonight"
     vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"} )
 end
 
 Mytheme()
