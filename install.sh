e2e6a1244ceda53d463d6eb2e63c188029c4f4b2
Ernest <musaubrian15@gmail.com>
2023-01-16 13:10:31 +0300

    chore: update tmux config

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 mkdir ~/scripts/
 cp ./tmux.conf ~/.tmux.conf
 cp ./cht.sh ~/scripts/
-
+mv .nvim ~/.config/nvim	
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
