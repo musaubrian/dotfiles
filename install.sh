b607451857aaf4749f0f5a44da2d4c1f1edc9b9f
Ernest <94367979+musaubrian@users.noreply.github.com>
2023-04-19 10:46:10 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,10 +15,8 @@ curl -sS https://starship.rs/install.sh | sh
 echo 'eval "$(starship init bash)"' > ~/.bashrc
 
 # Install Neovim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage --appimage-extract
-sudo mv squashfs-root / && sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+dnf copr enable agriffis/neovim-nightly
+dnf install -y neovim python3-neovim
 
 # Install Fuzzy Finder (fzf)
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
