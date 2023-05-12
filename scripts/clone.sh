2357a90f735b1835ba92387b53b7ab1db18c5026
musaubrian <musaubrian15@gmail.com>
2023-05-12 10:15:21 +0300

    chore: add gitlab support to clone script :)

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -2,16 +2,25 @@
 #
 
 PERSONAL_OR_WORK="$1"
-URL="$2"
-GIT_URL="git@github.com:"$URL
+GIT_HUB_OR_LAB="$2"
+URL="$3"
+CLONE_AS="$4"
+USAGE_INSTRUCTION="Usage:
+    clone [p/w] [lab/hub] username/repo clone_as"
 
-if [ "$PERSONAL_OR_WORK" == "" ]; then
-    echo "Destination not set"
-    echo "usage:
-            clone [p/w] username/repo"
-elif [ "$PERSONAL_OR_WORK" == "p" ]; then
+if [ "$GIT_HUB_OR_LAB" == "lab" ]; then
+    GIT_URL="git@gitlab.com:$URL $CLONE_AS"
+elif [ "$GIT_HUB_OR_LAB" == "hub" ]; then
+    GIT_URL="git@github.com:$URL $CLONE_AS"
+else
+    echo "$USAGE_INSTRUCTION"
+
+fi
+
+
+
+if [ "$PERSONAL_OR_WORK" == "p" ]; then
     echo "Switched to personal"
-    echo "$GIT_URL"
     cd ~/personal/ && `git clone $GIT_URL`
 elif [ "$PERSONAL_OR_WORK" == "w" ]; then
     echo "Switched to work"
