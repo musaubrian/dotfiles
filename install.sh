6e4f1e3a5f65635f4c8bde8a07447d0a3ed25e6a
musaubrian <musaubrian15@gmail.com>
2023-09-24 11:03:39 +0300

    add switch shell option

--- install.sh
+++ install.sh
@@ -47,6 +47,7 @@ sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # fish-like suggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+chsh -s $(which zsh)
 
 
 # Install Fuzzy Finder (fzf)
