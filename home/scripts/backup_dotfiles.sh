88f54918c0037c9d0535e3ce3ec66f47e6322b10
musaubrian <musaubrian15@gmail.com>
2024-11-12 09:29:01 +0300

    add cloudflare tunnel

--- home/scripts/backup_dotfiles.sh
+++ home/scripts/backup_dotfiles.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 ROOT_DIR="$HOME/personal/dotfiles"
-HOME_DIR="$ROOT_DIR/home/"
 KEYS_DIR="$ROOT_DIR/keys"
-CONFIG_DIR="$HOME_DIR/.config"
 STASH_DIR="$ROOT_DIR/stash"
 TO_DB="$STASH_DIR/db"
 WAKA_KEY="$STASH_DIR/wakatime/wakatime.cfg"
@@ -17,34 +15,6 @@ cp ~/.db/tinygo.db $TO_DB
 cp ~/.wakatime.cfg $WAKA_KEY
 echo "COPIED TO $STASH_DIR"
 
-cp ~/.profile $HOME_DIR
-cp ~/.aliases $HOME_DIR
-cp ~/.bashrc $HOME_DIR
-cp ~/.bash_completions $HOME_DIR
-cp ~/.gitconfig $HOME_DIR
-cp ~/.tmux.conf $HOME_DIR
-cp -r ~/.fonts $HOME_DIR
-cp -v /etc/X11/xorg.conf.d/90-touchpad.conf $HOME_DIR
-
-cp -v ~/.local/bin/yt-dlp "$HOME_DIR/.local/bin/"
-cp -v ~/.local/bin/name "$HOME_DIR/.local/bin/"
-cp -v ~/.local/bin/httpie.appimage "$HOME_DIR/.local/bin/"
-
-echo "COPIED .<files> TO $HOME_DIR"
-
-cp -r ~/.config/alacritty $CONFIG_DIR
-cp -r ~/.config/nvim $CONFIG_DIR
-cp -r ~/.config/kitty $CONFIG_DIR
-cp -r ~/.config/i3 $CONFIG_DIR
-cp -r ~/.config/Code/User/settings.json $CONFIG_DIR/Code/User/settings.json
-cp -r ~/.config/Code/User/keybindings.json $CONFIG_DIR/Code/User/keybindings.json
-cp -r ~/.config/wezterm $CONFIG_DIR
-cp -v ~/.config/starship.toml $CONFIG_DIR
-
-echo "COPIED TO $CONFIG_DIR"
-
-cp -r ~/scripts $HOME_DIR
-echo "COPIED scripts/"
 
 cd "$ROOT_DIR" || exit
 ansible-vault encrypt ./keys/* $STASH_DIR/db/* $STASH_DIR/wakatime/*
